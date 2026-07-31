5927c55377fac19b27f1601cd051b7b8fd106bd0
arshanforgithub <99459220+arshanforgithub@users.noreply.github.com>
2026-07-31 11:02:03 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,1 +1,17 @@
 
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println(getGreeting());
+    }
+
+    public static String getGreeting() {
+        return "Hello from Jenkins on EKS with Karpenter!";
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
